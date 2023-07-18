4394343593a2e71c085c80cdaffa120c55bf10f1
Basudev.Kirtania <Basudev.Kirtania@LDIGITBGL-L0133.godigit.corp>
2023-07-18 14:54:53 +0530

    Try and Multiple catch

---
+++ src/main/java/com/Digit/Test_Project/P1/Exception_Tryandcatch.java
@@ -0,0 +1,52 @@
+package com.Digit.Test_Project.P1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exception_Tryandcatch {
+	public static void main(String[] args) {
+		try {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("ENter NUmber1");
+			int num1 = sc.nextInt();
+
+			System.out.println("ENter NUmber1");
+			int num2 = sc.nextInt();
+
+			int res = num1 / num2;// Arithmatic Exception
+
+			System.out.println("ENter size");
+			int n = sc.nextInt();
+			int arr[] = new int[n];
+
+			int arr2[] = null;
+
+			System.out.println("ENter Position");
+			int pos = sc.nextInt();
+
+			System.out.println("ENter value");
+			int val = sc.nextInt();// IndexOutoofBound
+
+			arr[pos] = val;
+		} catch (ArithmeticException e1) {
+
+			System.out.println("ArithmeticException");
+		} catch (NegativeArraySizeException e2) {
+
+			System.out.println("NegativeArraySizeException");
+		} catch (ArrayIndexOutOfBoundsException e3) {
+
+			System.out.println("ArrayIndexOutOfBoundsException");
+		} catch (InputMismatchException e4) {
+
+			System.out.println("InputMismatchException");
+		} catch (NullPointerException e5) {
+
+			System.out.println("NullPointerException");
+		} catch (Exception e) {//Generic is the last catch block
+
+			System.out.println(" Generic Exception HAndler");
+		}
+
+	}
+}
