bf93fa7fbf8e5946356b9e5aa502d09a7873ca6b
Basudev.Kirtania <Basudev.Kirtania@LDIGITBGL-L0133.godigit.corp>
2023-07-18 12:24:36 +0530

    2nd

--- src/main/java/com/Digit/Test_Project/P1/TestingCode.java
+++ src/main/java/com/Digit/Test_Project/P1/TestingCode.java
@@ -2,6 +2,10 @@ package com.Digit.Test_Project.P1;
 
 public class TestingCode {
 public static void main(String[] args) {
-	
+	System.out.println("Hii");
+	System.out.println("Hii");
+	System.out.println("Hii");
+	System.out.println("Hii");
+
 }
 }
